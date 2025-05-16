bb8f2fd36ce311691bd2853701130bb54b466101
MiaAgnes <eaa24matd@students.eaaa.dk>
2025-05-16 19:54:26 +0200

    fix i sti

--- js/susi-spil.js
+++ js/susi-spil.js
@@ -216,7 +216,7 @@ function returnToPlanets() {
   
   
     setTimeout(() => {
-      window.location.href = '../html/end.html';
+      window.location.href = '../html/end-spil.html';
     }, 46000); 
   
     return;
